c169fa9948e7a8fa2e0c93f7ba9816b15e95c129
Tyler Kokoszka <techkoko77@gmail.com>
2018-11-11 13:30:15 -0500

    Context Loading Test
    The application context is tested to ensure the context loads and
    environment is non-null

--- src/test/java/com/tmk2003/zuultesting/ZuulTestingApplicationTests.java
+++ src/test/java/com/tmk2003/zuultesting/ZuulTestingApplicationTests.java
@@ -2,15 +2,28 @@ package com.tmk2003.zuultesting;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ZuulTestingApplicationTests {
 
+	@Autowired
+	private Environment env;
+
+	/**
+	 * Ensure the Zuul application can actually load its context
+	 */
 	@Test
 	public void contextLoads() {
+		assertTrue("Application loads context properly", true);
+		assertNotNull(env);
 	}
 
 }
