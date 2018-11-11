088134b1cba3270464b947001f7da0d56ac68507
Tyler Kokoszka <techkoko77@gmail.com>
2018-11-11 11:43:06 -0500

    Initial Commit

---
+++ src/main/java/com/tmk2003/zuultesting/ZuulTestingApplication.java
@@ -0,0 +1,12 @@
+package com.tmk2003.zuultesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulTestingApplication.class, args);
+	}
+}
